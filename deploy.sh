130ab04d3c3dc27b917a36a26b0c3479aa3f39a9
unknown <MYOUNG-JAE-KIM>
2025-04-11 15:43:19 +0900

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+#커밋 메시지 입력 받기
+read -p "커밋 메시지를 입력하세요: " user_message
+
+#KST 시간 구하기 (UTC+9)
+curtent_time=$(TZ=Asia/Seoul date "+%Y-%m-%d %H:%M:%S")
+
+#전체 메시지 구성
+commit_message="$user_message | $current_time (KST)"
+
 git add .
 git commit -m "updated"
 git push
